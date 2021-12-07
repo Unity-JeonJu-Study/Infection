63ef8e94d4e52a27b76226131e69375aa5d6b03c
sadoe3 <sadoe3@gmail.com>
2021-12-07 13:37:36 +0900

    update save load manager

--- Assets/Scripts/Managers/SaveLoadManager.cs
+++ Assets/Scripts/Managers/SaveLoadManager.cs
@@ -1,18 +1,48 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SaveLoadManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public static SaveLoadManager instance;
+
+    public List<SaveData> data;
+    public int maxSaveSlot;
+
+    private string defaultSavePath;
+
+    private void Awake() {
+        instance = this;
+
+        defaultSavePath = "SaveData/";
+        data = new List<SaveData>();
+        maxSaveSlot = 5;
+
+        LoadAllData();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void LoadAllData() {
+        data.Clear();
+
+        for(int curIndex = 0; curIndex < maxSaveSlot; curIndex++) {
+            if(ES3.KeyExists(curIndex.ToString(), defaultSavePath + curIndex + ".es3"))
+                data.Add(ES3.Load<SaveData>(curIndex.ToString(), defaultSavePath + curIndex + ".es3"));
+            else
+                data.Add(new SaveData(true));
+        }
+    }
+
+    public SaveData GetLatestData() {
+        int latestDataIndex = 0;
+        for(int curIndex = 1; curIndex < maxSaveSlot; curIndex++) {
+            if(DateTime.Compare(data[latestDataIndex].saveDateTime, data[curIndex].saveDateTime) < 0) {
+                latestDataIndex = curIndex;
+            }
+        }
+        return data[latestDataIndex];
+    }
+
+    public void SaveData(int slotIndex, SaveData inputData) {
+        ES3.Save(slotIndex.ToString(), inputData, defaultSavePath + slotIndex + ".es3");
     }
 }
---
+++ Assets/Scripts/SaveLoad/SaveData.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public struct SaveData
+{
+    public bool isLoaded;
+    public string stageName;
+    public DateTime saveDateTime;
+    public Vector3 respawnPoint;
+    //public List<AvailableWeapon> savedAvailableWeapons;
+    public int savedHealth;
+
+    public SaveData(string inputStageName, DateTime inputSaveDateTime, Vector3 inputRespawnPoint, /*List<AvailableWeapon> inputSavedAvailableWeapon,*/ int inputSavedHealth) {
+        isLoaded = true;
+        
+        stageName = inputStageName;
+        saveDateTime = inputSaveDateTime;
+        respawnPoint = inputRespawnPoint;
+
+        //savedAvailableWeapons = inputSavedAvailableWeapon;
+        savedHealth = inputSavedHealth;
+    }
+
+    public SaveData(bool trashValue) {
+        isLoaded = false;
+
+        stageName = "";
+        saveDateTime = DateTime.MinValue;
+        respawnPoint = Vector3.one;
+        //savedAvailableWeapons = null;
+        savedHealth = -1;
+    }
+
+    public string GetSaveSlotName() {
+        if(isLoaded) 
+            return stageName + " " + saveDateTime.ToString("yyyy/mm/dd  hh/mm/ss");
+        else 
+            return "empty";
+    }
+}

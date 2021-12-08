4ea15112b65641b074047bb53a2e00f4f52038ff
Kyle <sadoe3@gmail.com>
2021-12-08 18:36:55 +0900

    add game over popup

---
+++ Assets/Scripts/UI/GameOverPopup.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class GameOverPopup : MonoBehaviour
+{
+    public void OnClickRestart() {
+       // restart
+    }
+
+    public void OnClickMainMenu() {
+        //UIManager.instance.EnableLoadPopup();
+        // scene manager part
+    }
+}

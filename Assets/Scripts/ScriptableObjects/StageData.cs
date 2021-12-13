a048764a77a7035f6016dc6d1b674471457e7539
Jacob <tldk47@hanmail.net>
2021-12-13 15:47:31 +0900

    edit stageData
    add code
    - generateStage

--- Assets/Scripts/ScriptableObjects/StageData.cs
+++ Assets/Scripts/ScriptableObjects/StageData.cs
@@ -7,11 +7,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "StageData", menuName = "Data/Stage")]
 public class StageData : SerializedScriptableObject
 {
-    public Dictionary<GameStage, Stage> data;
-    public StageData()
-    {
-        data = new Dictionary<GameStage, Stage>();
-    }
+    public Dictionary<GameStage, Stage> data = new Dictionary<GameStage, Stage>();
 
     [Button("Generate Stage")]
     private void GenerateStage()
@@ -20,19 +16,13 @@ public class StageData : SerializedScriptableObject
         {
             if (!data.ContainsKey(gameStage))
                 data.Add(gameStage, new Stage());
-
-            data[gameStage].gamePrefab = Resources.Load<GameObject>("Data/Level/" + gameStage);
-            data[gameStage].quest = Resources.Load<QuestList>("Data/Quest/_" + gameStage);
-            data[gameStage].bgm = Resources.Load("Sound Player/BGM/" + gameStage).ToString();
+            if (File.Exists("Assets/Resources/Data/Level/" + gameStage + ".prefab"))
+                data[gameStage].gamePrefab = Resources.Load<GameObject>("Data/Level/" + gameStage);
+            if (File.Exists("Assets/Resources/Data/Quest/_" + gameStage + ".asset"))
+                data[gameStage].quest = Resources.Load<QuestList>("Data/Quest/_" + gameStage);
+            if (File.Exists("Assets/Resources/Sound Player/BGM/" + gameStage + ".wav"))
+                data[gameStage].bgm = Resources.Load("Sound Player/BGM/" + gameStage).ToString();
         }
-
-
-        // foreach (var stage in data)
-        // {
-        //     stage.Value.gamePrefab = Resources.Load<GameObject>("Data/Level/" + stage.Key);
-        //     stage.Value.quest = Resources.Load<QuestList>("Data/Quest/_" +  stage.Key);
-        //     stage.Value.bgm = Resources.Load("Sound Player/BGM/" +  stage.Key).ToString();
-        // }
     }
 
     public class Stage
@@ -43,14 +33,5 @@ public class StageData : SerializedScriptableObject
         public float limitTime;
         public int minSlimCount;
         public int maxSlimCount;
-
-        [Button("Load Quest")]
-        private void LoadQuest()
-        {
-            if (File.Exists("Assets/Resources/Data/Quest/_" + gamePrefab.name + ".asset"))
-                quest = Resources.Load<QuestList>("Data/Quest/_" + gamePrefab.name);
-            else
-                Debug.LogError("File does not exist");
-        }
     }
 }

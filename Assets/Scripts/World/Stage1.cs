5345ebfbaa18dd75f5dca5a6d4e33c1c2fe50cbd
shallet44 <shallet44@naver.com>
2021-12-21 03:43:30 +0900

    stage01 수정본입니다.

--- Assets/Scripts/World/Stage1.cs
+++ Assets/Scripts/World/Stage1.cs
@@ -1,9 +1,9 @@
-using System;
 using UnityEngine;
 
 public class Stage1 : MonoBehaviour
 {
     public GameObject finalDoor;
+    public bool flag;
     private void Start()
     {
         if(finalDoor != null)
@@ -12,12 +12,15 @@ public class Stage1 : MonoBehaviour
 
     private void Update()
     {
+        if (flag)
+            return;
         var x = InGameUIManager.instance.GetRescuedSlimeCount();
 
         if (GameManager.Instance.stageData.data[GameStage.Stage1].minSlimeCount <= x)
         {
             finalDoor.SetActive(true);
             GameManager.Instance.StartStageClear();
+            flag = true;
         }
     }
 }
